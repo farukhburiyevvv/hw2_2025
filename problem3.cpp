19d61f034dd0394c3a883e232ae675122e910578
farrukh <f.buriyev@newuu.uz>
2025-11-13 20:53:48 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -8,18 +8,30 @@ int main() {
     string cmd;
     for (int i = 0; i < q; ++i) {
         cin >> cmd;
-        if (cmd == "insert") { long long x; cin >> x; A.push_back(x); }
-        else if (cmd == "get") { string tmp; cin >> tmp; 
-            if (A.empty()) cout << "EMPTY\n"; 
-            else cout << *max_element(A.begin(), A.end()) << "\n"; 
+        if (cmd == "insert") {
+            long long x; cin >> x;
+            A.push_back(x);
         }
-        else if (cmd == "extract") { string tmp; cin >> tmp; 
-            if (A.empty()) cout << "EMPTY\n"; 
-            else { 
-                auto it = max_element(A.begin(), A.end()); 
-                cout << *it << "\n"; 
-                A.erase(it); 
-            } 
+        else if (cmd == "get") {
+            string tmp; cin >> tmp; // max
+            if (A.empty()) cout << "EMPTY\n";
+            else {
+                long long mx = A[0];
+                for (auto v : A) if (v > mx) mx = v;
+                cout << mx << "\n";
+            }
+        }
+        else if (cmd == "extract") {
+            string tmp; cin >> tmp; // max
+            if (A.empty()) cout << "EMPTY\n";
+            else {
+                long long mx = A[0];
+                int idx = 0;
+                for (int j = 0; j < (int)A.size(); ++j)
+                    if (A[j] > mx) { mx = A[j]; idx = j; }
+                cout << mx << "\n";
+                A.erase(A.begin() + idx); // remove first occurrence
+            }
         }
         else if (cmd == "size") cout << A.size() << "\n";
     }

4f9ca8f5a9ae15e8f8628b56fe4048879a4b5bc1
farrukh <f.buriyev@newuu.uz>
2025-11-13 20:55:20 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -11,28 +11,26 @@ int main() {
         if (cmd == "insert") {
             long long x; cin >> x;
             A.push_back(x);
-        }
-        else if (cmd == "get") {
-            string tmp; cin >> tmp; // max
+        } else if (cmd == "get") {
+            string tmp; cin >> tmp;
             if (A.empty()) cout << "EMPTY\n";
             else {
-                long long mx = A[0];
-                for (auto v : A) if (v > mx) mx = v;
+                long long mx = LLONG_MIN;
+                for (auto v : A) mx = max(mx, v);
                 cout << mx << "\n";
             }
-        }
-        else if (cmd == "extract") {
-            string tmp; cin >> tmp; // max
+        } else if (cmd == "extract") {
+            string tmp; cin >> tmp;
             if (A.empty()) cout << "EMPTY\n";
             else {
-                long long mx = A[0];
-                int idx = 0;
-                for (int j = 0; j < (int)A.size(); ++j)
+                long long mx = LLONG_MIN;
+                int idx = -1;
+                for (int j = 0; j < (int)A.size(); ++j) {
                     if (A[j] > mx) { mx = A[j]; idx = j; }
+                }
                 cout << mx << "\n";
-                A.erase(A.begin() + idx); // remove first occurrence
+                A.erase(A.begin() + idx);
             }
-        }
-        else if (cmd == "size") cout << A.size() << "\n";
+        } else if (cmd == "size") cout << A.size() << "\n";
     }
 }

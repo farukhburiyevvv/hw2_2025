7acd38b1184f4c403496ce24616fee1a8be00db5
farrukh <f.buriyev@newuu.uz>
2025-11-13 20:57:59 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -4,35 +4,34 @@ using namespace std;
 int main() {
     int q;
     cin >> q;
-    vector<long long> pq;
-    for (int i = 0; i < q; ++i) {
+    vector<long long> A;
+    while (q--) {
         string cmd;
         cin >> cmd;
         if (cmd == "insert") {
-            long long x;
-            cin >> x;
-            pq.push_back(x);
+            long long x; cin >> x;
+            A.push_back(x);
         } else if (cmd == "get") {
-            string tmp; cin >> tmp; // max
-            if (pq.empty()) cout << "EMPTY\n";
+            string tmp; cin >> tmp;
+            if (A.empty()) cout << "EMPTY\n";
             else {
-                long long mx = pq[0];
-                for (auto x : pq) if (x > mx) mx = x;
+                long long mx = A[0];
+                for (auto x : A) if (x > mx) mx = x;
                 cout << mx << "\n";
             }
         } else if (cmd == "extract") {
-            string tmp; cin >> tmp; // max
-            if (pq.empty()) cout << "EMPTY\n";
+            string tmp; cin >> tmp;
+            if (A.empty()) cout << "EMPTY\n";
             else {
-                long long mx = pq[0];
+                long long mx = A[0];
                 int idx = 0;
-                for (int j = 1; j < (int)pq.size(); ++j)
-                    if (pq[j] > mx) { mx = pq[j]; idx = j; }
+                for (int i = 0; i < A.size(); ++i)
+                    if (A[i] > mx) { mx = A[i]; idx = i; }
                 cout << mx << "\n";
-                pq.erase(pq.begin() + idx);
+                A.erase(A.begin() + idx);
             }
         } else if (cmd == "size") {
-            cout << pq.size() << "\n";
+            cout << A.size() << "\n";
         }
     }
 }
